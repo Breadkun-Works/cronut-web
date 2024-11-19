16b8191edb69adafb44ac58484af4222a6221d55
aesop0817 <aesop0817@douzone.com>
2024-11-19 10:23:38 +0900

    add: icon rest images(1)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     compiler: {
-        styledComponents: true, // SSR 지원 활성화
-    },
+        styledComponents: true // SSR 지원 활성화
+    }
 };
 
 export default nextConfig;

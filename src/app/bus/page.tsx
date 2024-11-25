fda7af3d591cd0c0f45ed2743a879e8f770c33b0
aesop0817 <aesop0817@douzone.com>
2024-11-25 15:54:08 +0900

    add: modify Bus component

---
+++ src/app/bus/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Bus from '@/components/page/bus';
+
+const BusPage = () => {
+    return (
+        <div>
+            {/*<h1>Welcome to the Bus Page</h1>*/}
+            <Bus /> {/* Bus 컴포넌트 사용 */}
+        </div>
+    );
+};
+
+export default BusPage;

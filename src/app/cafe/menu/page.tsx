826193a03f89a3a998cd1ffbd42858e286f72e40
aesop0817 <aesop0817@douzone.com>
2025-03-03 22:54:56 +0900

    refactor: styled-components=>emotion/react, emotion/css로 변경

--- src/app/cafe/menu/page.tsx
+++ src/app/cafe/menu/page.tsx
@@ -1,7 +1,31 @@
 'use client';
 
+import { Tab, Tabs } from '@mui/material';
+import { CafeMenuData } from '@/data';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+
 const CafeMenu = () => {
-    return <div>?</div>;
+    const [tabValue, setTabValue] = useState(0);
+    const handleTabChange = (event: React.SyntheticEvent, newTabValue: number) => {
+        setTabValue(newTabValue);
+    };
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Tabs centered value={tabValue} onChange={handleTabChange}>
+                {CafeMenuData.map((cafeMenu, cafeMenuIdx) => {
+                    return (
+                        <Tab
+                            key={cafeMenuIdx}
+                            label={cafeMenu.name}
+                            value={cafeMenuIdx}
+                            sx={cafeMenu.sx} // cafeMenu.sx로 접근하여 스타일을 적용
+                        />
+                    );
+                })}
+            </Tabs>
+        </div>
+    );
 };
 
 export default CafeMenu;

6d31982baa41e162644abb850e3e92604feaf052
aesop0817 <aesop0817@douzone.com>
2025-07-08 17:48:10 +0900

    add: 네이버 analytics, meta 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,25 @@ import { DEFAULT_OG_IMAGE } from '@/data';
 import Script from 'next/script';
 
 export const metadata: Metadata = {
+    robots: 'index, follow',
+    keywords: [
+        '빵돌이',
+        '더존ICT',
+        '더존',
+        '생활정보',
+        '버스',
+        '시간',
+        '식당',
+        '식단',
+        '날씨',
+        '회식장소',
+        '맛집',
+        '카페'
+    ],
+    // 오픈그래프
     openGraph: {
+        title: 'BBANGDORI',
+        url: 'https://breadkun.com/',
         images: [DEFAULT_OG_IMAGE],
         type: 'website',
         siteName: 'BBANGDORI'
@@ -18,12 +36,19 @@ export const metadata: Metadata = {
         '더존ICT의 구내식당 식단, 통근 버스의 실시간 도착 시간, 오늘의 빵, 사내 카페 메뉴, 날씨 등 다양한 생활 정보를 안내하는 더존 빵돌이 웹 서비스입니다.'
 };
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <html lang="ko" className={nanumFonts.variable}>
             <head title={'더존 빵돌이 | 다양한 더존ICT 생활 정보'}>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="theme-color" content="#212529" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="naver-site-verification" content="114e2081d9285565efb98f83edf15a821be642ef" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
+                <link rel="apple-touch-icon" sizes="512x512" href="/logo512.png" />
+                {/* Google Analytics */}
                 <Script src={`https://www.googletagmanager.com/gtag/js?id=G-92E8PST3BH`} strategy="afterInteractive" />
                 <Script id="gtag-init" strategy="afterInteractive">
                     {`window.dataLayer = window.dataLayer || [];
@@ -31,11 +56,25 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
                             gtag('js', new Date());
                             gtag('config', 'G-92E8PST3BH');`}
                 </Script>
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
+                {/* KAKAO API */}
+                <Script
+                    src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&libraries=services`}
+                    strategy="afterInteractive"
+                />
             </head>
             <body>
                 <ClientLayout>{children}</ClientLayout>
+                {/* NAVER Analytics */}
+                <Script src="//wcs.naver.net/wcslog.js" strategy="afterInteractive" />
+                <Script id="naver-analytics" strategy="afterInteractive">
+                    {`
+                        if (!wcs_add) var wcs_add = {}; 
+                        wcs_add['wa'] = '2bc1d10b4c655c';
+                        if (window.wcs) {
+                            wcs_do();
+                        }
+                    `}
+                </Script>
             </body>
         </html>
     );

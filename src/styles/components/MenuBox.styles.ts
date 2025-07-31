7ddb1b709756f9284e689013a838369b652a9984
gimjihye <jihyek828@douzone.com>
2025-07-31 10:17:31 +0900

    style: 모바일 메뉴 수정

---
+++ src/styles/components/MenuBox.styles.ts
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export const MenuBoxWrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 99;
+`;
+
+export const MenuCloseButton = styled.button`
+    position: fixed;
+    top: 10px;
+    right: 15px;
+
+    svg {
+        width: 40px;
+        height: 40px;
+    }
+`;
+
+export const MobileNav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+
+        li {
+            text-align: center;
+            font-size: 38px;
+        }
+    }
+`;
+
+export const MenuItem = styled.a<{ active?: boolean }>`
+    display: inline-block;
+    opacity: ${({ active }) => (active ? 1 : 0.5)};
+    color: #fff;
+
+    ${({ active }) =>
+        active &&
+        css`
+            font-weight: bold;
+            border-bottom: 1px solid #fff;
+        `}
+`;
